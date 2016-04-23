d7a8c560dc7be80bee57e40d3d73adc7d37852a9
Simon Mika <simon@mika.se>
2016-04-23 22:45:41 +0200

    Added visualization.

--- source/PhoneControl.Demo/Program.cs
+++ source/PhoneControl.Demo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Draw = Kean.Draw;
+using Geometry2D = Kean.Math.Geometry2D;
+using Single = Kean.Math.Single;
 
 namespace PhoneControl.Demo
 {
@@ -6,16 +9,36 @@ namespace PhoneControl.Demo
 	{
 		public static void Main (string[] args)
 		{
+			float rotationX = 0;
+			float rotationZ = 0;
 			Kean.Error.Log.CatchErrors = false;
+
+			Draw.OpenGL.Window window = new Draw.OpenGL.Window();
+			var background = Draw.Raster.Bgr.Open("./background.jpg");
+			window.Draw += surface =>
+			{
+				surface.Push(Geometry2D.Single.Transform.CreateRotation(rotationZ) * Geometry2D.Single.Transform.CreateTranslation(0, rotationX / 3.1415f * 5000));
+				surface.Clear();
+				surface.Draw(background, (Geometry2D.Single.Point)((Geometry2D.Single.Size)background.Size / -2));
+				surface.Pop();
+			};
+
 			var server = new Server("file:///./Client/");
 			int deviceCount = 0;
 			server.DeviceCreated += device =>
 			{ 
 				var number = deviceCount++;
-				device.OrientationChanged += orientation => Console.WriteLine(number + ": " + orientation);
+				device.OrientationChanged += orientation => {
+					rotationX = Single.ToRadians(orientation.Gamma);
+					rotationZ = -Single.ToRadians(orientation.Alpha);
+					window.Invalidate();
+					Console.WriteLine(number + ": " + orientation);
+				};
 			};
+			server.Start(8080);
 			Console.WriteLine ("Server Listening on port 8080.");
-			server.Run(8080);
+			window.Visible = true;
+			window.Run();
 		}
 	}
 }
